e4e6218b8d0ad3bd258cb923c4f467c950bc4c3a
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-03-16 21:18:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// Multiply num1 and num2
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate the remainder of num3 divided by num4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Find the highest number in a set
+const max = Math.max(1, 5, 10, 15, 20);
